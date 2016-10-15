53c99bf107b43f6230e0d6fe27822c9531441bad
tomaka <htomaka@treeptik.fr>
2016-10-15 18:54:08 +0200

    split webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require ('webpack');
+var path = require ('path');
+var merge = require ('webpack-merge')
+var APP_DIR = path.resolve (__dirname + '/app');
+var BUILD_DIR = path.resolve (__dirname + '/build');
+var config;
+var common = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                loader: 'babel',
+                include: APP_DIR,
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    }
+};
 
-var APP_DIR = path.resolve(__dirname + '/app');
-var BUILD_DIR = path.resolve(__dirname + '/build');
 
-
-var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  devtool: 'source-map',
-  devServer: {
-    inline: true,
-    contentBase: BUILD_DIR,
-    port: 3000
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        loader: 'babel',
-        include: APP_DIR,
-        query: {
-          presets: ["es2015", "react"]
+if( process.env.npm_lifecycle_event === 'dev' ) {
+    config = merge (common, {
+        devServer: {
+            inline: true,
+            contentBase: BUILD_DIR,
+            port: 3000
         }
-      }
-    ]
-  }
+    });
+} else if( process.env.npm_lifecycle_event === 'prod' ) {
+    config = merge (common, {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin ({
+                minimize: true
+            })
+        ]
+    });
 }
 
-if(process.env.environment === 'DEV'){
-  console.log(process.env)
-  config = Object.assign(config, require('./webpack.config.dev'));
-} else if(process.env.environment === 'PROD'){
-  console.log(process.env)
-  config = Object.assign(config, require('./webpack.config.prod'));
-}
 
 module.exports = config;
